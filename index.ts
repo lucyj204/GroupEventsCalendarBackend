ebea0a541c10b0db2e0185191cbf2fa133805eeb
Lucy Joyce <lucyj20490@gmail.com>
2021-10-24 17:49:31 +0100

    Convert to TypeScript

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/bananas', (req, res) => {
+  res.send('mmmm bananas!!!!!!')
 })
 
 app.listen(port, () => {
