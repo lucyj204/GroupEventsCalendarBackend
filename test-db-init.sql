bac96a35f47df0f9054b8e64d4b7c677c43728b5
Lucy Joyce <lucyj20490@gmail.com>
2021-11-18 16:07:43 +0000

    Update test-db to fix issue with getting events from groups

--- test-db-init.sql
+++ test-db-init.sql
@@ -24,17 +24,17 @@ INSERT INTO "session" ("id", "user", "expiry") VALUES ('abc536573169576518375758
 
 CREATE TABLE "event" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "event_name" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "location" TEXT NOT NULL,
     "start_date" TEXT NOT NULL,
     "end_date" TEXT NOT NULL,
-    "group_id" REFERENCES group(id)
+    "group_id" TEXT REFERENCES "group"("id")
 );
 
-INSERT INTO "event" ("id", "event_name", "location", "start_date", "end_date", "group_id") VALUES ('342561840597', 'Spearhead', 'E1', '11/20/21 5:00 PM', '11/20/21 11:00 PM', '321413513');
-INSERT INTO "event" ("id", "event_name", "location", "start_date", "end_date", "group_id") VALUES ('342561840876', 'Kings of The Rollers', 'Printworks', '11/25/21 12:00 PM', '11/25/21 11:00 PM', '321413513');
-INSERT INTO "event" ("id", "event_name", "location", "start_date", "end_date", "group_id") VALUES ('342561840597', 'Ham Samwich', 'Fortune of War', '12/2/21 5:00 PM', '12/2/21 11:00 PM', '4134324234');
-INSERT INTO "event" ("id", "event_name", "location", "start_date", "end_date", "group_id") VALUES ('342561840597', 'Pub Night', 'The Eagle Tavern', '11/20/21 7:00 PM', '11/20/21 1:00 AM', '4134324234');
-INSERT INTO "event" ("id", "event_name", "location", "start_date", "end_date", "group_id") VALUES ('342561840597', 'Dinner', '22 Watts Avenue', '11/27/21 5:00 PM', '11/27/21 11:00 PM', '43124324');
-INSERT INTO "event" ("id", "event_name", "location", "start_date", "end_date", "group_id") VALUES ('342561840597', 'Mixed Ability Match', 'Hackney Playing Field', '11/21/21 11:00 AM', '11/21/21 2:00 PM', '32143242341');
+INSERT INTO "event" ("id", "name", "location", "start_date", "end_date", "group_id") VALUES ('342561840597', 'Spearhead', 'E1', '11/20/21 5:00 PM', '11/20/21 11:00 PM', '321413513');
+INSERT INTO "event" ("id", "name", "location", "start_date", "end_date", "group_id") VALUES ('342561840876', 'Kings of The Rollers', 'Printworks', '11/25/21 12:00 PM', '11/25/21 11:00 PM', '321413513');
+INSERT INTO "event" ("id", "name", "location", "start_date", "end_date", "group_id") VALUES ('342561265378', 'Ham Samwich', 'Fortune of War', '12/2/21 5:00 PM', '12/2/21 11:00 PM', '4134324234');
+INSERT INTO "event" ("id", "name", "location", "start_date", "end_date", "group_id") VALUES ('342561814293', 'Pub Night', 'The Eagle Tavern', '11/20/21 7:00 PM', '11/20/21 1:00 AM', '4134324234');
+INSERT INTO "event" ("id", "name", "location", "start_date", "end_date", "group_id") VALUES ('34256184309187', 'Dinner', '22 Watts Avenue', '11/27/21 5:00 PM', '11/27/21 11:00 PM', '43124324');
+INSERT INTO "event" ("id", "name", "location", "start_date", "end_date", "group_id") VALUES ('34256184052731', 'Mixed Ability Match', 'Hackney Playing Field', '11/21/21 11:00 AM', '11/21/21 2:00 PM', '32143242341');
 
